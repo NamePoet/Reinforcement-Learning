6ae3071305120e124ff353597cdd2dd45bb5bfcc
NamePoet <y1813880702@163.com>
2023-12-28 23:23:22 +0800

    add update-q-func

--- CartPole_V1/QNetwork.py
+++ CartPole_V1/QNetwork.py
@@ -23,3 +23,18 @@ class QLearningAgent:
                 state_tensor = torch.FloatTensor(state).unsqueeze(0)
                 q_values = self.q_network(state_tensor)
                 return np.argmax(q_values.numpy())
+
+    def update_q_network(self, state, action, reward, next_state, done):
+        state_tensor = torch.FloatTensor(state).unsqueeze(0)
+        next_state_tensor = torch.FloatTensor(next_state).unsqueeze(0)
+        q_values = self.q_network(state_tensor)
+        next_q_values = self.target_q_network(next_state_tensor).detach()
+
+        target = q_values.clone()
+        target[0][action] = reward + self.gamma * torch.max(next_q_values).item() * (1 - done)
+
+        loss = nn.MSELoss()(q_values, target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
