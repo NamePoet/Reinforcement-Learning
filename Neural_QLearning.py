db39cd40e548e87254d54d790e668b7cd70de240
NamePoet <y1813880702@163.com>
2024-01-05 23:44:27 +0800

    build neural net of Q-function

---
+++ Neural_QLearning.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+from Gridworld import Gridworld
+import random
+from matplotlib import pyplot as plt
+
+l1 = 64
+l2 = 150
+l3 = 100
+l4 = 4
+model = torch.nn.Sequential(
+    torch.nn.Linear(l1, l2),
+    torch.nn.ReLU(),
+    torch.nn.Linear(l2, l3),
+    torch.nn.ReLU(),
+    torch.nn.Linear(l3,l4)
+)
+loss_fn = torch.nn.MSELoss()
+learning_rate = 1e-3
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+gamma = 0.9
+epsilon = 1.0
